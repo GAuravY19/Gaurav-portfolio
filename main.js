ff07417471b145b7981a29fce17b95a61f412864
gaurav <gauravy1905@gmail.com>
2026-03-04 10:15:18 +0530

    Made the contact form working

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
-const form = document.getElementById("contact-form");
-
-form.addEventListener("submit", function(e){
-
-e.preventDefault();
-
-alert("Thanks for reaching out! I will contact you soon.");
-
-form.reset();
-
-});
-
-
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
+});
+
+
+(function () {
+    emailjs.init("RMSn-QOPeOVeZCA0y");
+})();
+
+const form = document.getElementById("contact-form");
+const messageBox = document.getElementById("form-message");
+
+form.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    emailjs.sendForm("service_m19vr7o", "template_tdyubnc", this)
+        .then(function () {
+
+            messageBox.className = "form-message success";
+            messageBox.innerText = "Message sent successfully! I will get back to you soon.";
+
+            form.reset();
+
+        })
+        .catch(function () {
+
+            messageBox.className = "form-message error";
+            messageBox.innerText = "Something went wrong. Please try again.";
+
+        });
+
 });
